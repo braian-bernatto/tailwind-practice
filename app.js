672b749f89ffc50c94939a9c1354a9f73a145905
Bernatto <braianbernatto@hotmail.com>
2021-10-20 18:32:40 -0300

    download button on

--- app.js
+++ app.js
@@ -35,62 +35,62 @@ function cargarDatos(){
 
 
 ////////////////////////////////////////carrito compras////////////////////////////
-const productos = [{nombre: 'frutilla 🍓', icono: '', }, {nombre: 'banana 🍌', 
-icono: ''}, {nombre: 'manzana 🍏', icono: ''}]
+// const productos = [{nombre: 'frutilla 🍓', icono: '', }, {nombre: 'banana 🍌', 
+// icono: ''}, {nombre: 'manzana 🍏', icono: ''}]
 
-const fragmentMercado = document.createDocumentFragment()
-const templateMercado = document.querySelector('#frutasTemplate')
-const mercado = document.querySelector('#mercado')
+// const fragmentMercado = document.createDocumentFragment()
+// const templateMercado = document.querySelector('#frutasTemplate')
+// const mercado = document.querySelector('#mercado')
 
-productos.forEach(({nombre, icono}) => {
-  const clone = templateMercado.content.firstElementChild.cloneNode(true)
-  clone.querySelector('#producto-nombre').textContent = nombre
-  clone.querySelector('#producto-icono').innerHTML = icono
-  clone.querySelector('#producto-btn').addEventListener('click', actualizarCarrito)
-  fragmentMercado.appendChild(clone)
-})
+// productos.forEach(({nombre, icono}) => {
+//   const clone = templateMercado.content.firstElementChild.cloneNode(true)
+//   clone.querySelector('#producto-nombre').textContent = nombre
+//   clone.querySelector('#producto-icono').innerHTML = icono
+//   clone.querySelector('#producto-btn').addEventListener('click', actualizarCarrito)
+//   fragmentMercado.appendChild(clone)
+// })
 
-mercado.appendChild(fragmentMercado)
+// mercado.appendChild(fragmentMercado)
 
 
-////////////////////////////// carrito template/////////////////////////////
-const carritoLista = document.querySelector('#carrito')
-const carritoFragment = document.createDocumentFragment('div')
-const templateCarrito = document.querySelector('#carrito-template')
-const carrito = []
+// ////////////////////////////// carrito template/////////////////////////////
+// const carritoLista = document.querySelector('#carrito')
+// const carritoFragment = document.createDocumentFragment('div')
+// const templateCarrito = document.querySelector('#carrito-template')
+// const carrito = []
 
-function agregarProducto() {
-  let clone
-  carrito.forEach(({nombre, cantidad}) => {
-    clone = templateCarrito.content.firstElementChild.cloneNode(true)  
-    clone.querySelector('#carrito-nombre').textContent = nombre
-    clone.querySelector('#carrito-cantidad').textContent = cantidad
-    carritoFragment.appendChild(clone)
-  })
-  carritoLista.appendChild(carritoFragment)
-}
+// function agregarProducto() {
+//   let clone
+//   carrito.forEach(({nombre, cantidad}) => {
+//     clone = templateCarrito.content.firstElementChild.cloneNode(true)  
+//     clone.querySelector('#carrito-nombre').textContent = nombre
+//     clone.querySelector('#carrito-cantidad').textContent = cantidad
+//     carritoFragment.appendChild(clone)
+//   })
+//   carritoLista.appendChild(carritoFragment)
+// }
 
-function actualizarCarrito(e) {
-  let producto = e.target.parentElement.querySelector('#producto-nombre').textContent
-  if (!carrito.some(item => item.nombre === producto)) {
-    carrito.push({nombre: producto, cantidad: 1})
-   }else{
-     carrito.forEach(item => {
-        if (item.nombre === producto) {
-         item.cantidad ++
-         return
-        }
-     })
-   }
+// function actualizarCarrito(e) {
+//   let producto = e.target.parentElement.querySelector('#producto-nombre').textContent
+//   if (!carrito.some(item => item.nombre === producto)) {
+//     carrito.push({nombre: producto, cantidad: 1})
+//    }else{
+//      carrito.forEach(item => {
+//         if (item.nombre === producto) {
+//          item.cantidad ++
+//          return
+//         }
+//      })
+//    }
     
-  limpiarHtml()
-  agregarProducto()
-}
+//   limpiarHtml()
+//   agregarProducto()
+// }
 
-function limpiarHtml() {
-  while (carritoLista.firstChild) {
-    carritoLista.removeChild(carritoLista.firstChild)
-  }
-}
+// function limpiarHtml() {
+//   while (carritoLista.firstChild) {
+//     carritoLista.removeChild(carritoLista.firstChild)
+//   }
+// }
 
 ////////////////////////////// carrito template/////////////////////////////
